80a73d035ab1b22f1f7a8632e493c3d6e29c6fb9
pipapa <yezhengmaolove@gmail.com>
2017-08-28 22:37:52 +0800

    json设置

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,5 @@
-from flask import jsonify
+import json
+from flask import jsonify,url_for
 from . import api
 from .. import db
 from ..models import User,Article,Category,Tag
@@ -7,6 +8,13 @@ from ..models import User,Article,Category,Tag
 @api.route('/api/articles')
 def get_articles():
     articles = Article.query.all()
+    jsonObj = {'status':'OK'} 
     for article in articles:
-    return 'test'
+        info={}
+        info['title']=article.title
+        info['url']=url_for('user.articles_index',article_id=article.id)
+        info['public_time']=article.public_time.strftime('%B %d %Y - %H:%M:%S')
+        info['update_time']=article.update_time.strftime('%B %d %Y - %H:%M:%S')
+        jsonObj[str(article.id)]=info
+    return jsonify(jsonObj)
 # 
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     DEBUG=True
+    JSON_SORT_KEYS=False
     SQLALCHEMY_COMMIT_ON_TEARDOWN=True
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(basedir,'FrogBlog.db')
