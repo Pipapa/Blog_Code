88d9e3fc6710f592b527126b3c0411c46a7f4de8
pipapa <yezhengmaolove@gmail.com>
2017-10-01 00:02:21 +0800

    test:交由Vue处理路由/前后端彻底分离

--- frogblog/frontend/views.py
+++ frogblog/frontend/views.py
@@ -6,47 +6,6 @@ from frogblog.models import User,Article,Category,Tag,Comment
 
 frontend = Blueprint('user',__name__)
 
-# 路由跳转
-@frontend.route('/posts/pages')
-@frontend.route('/posts')
-@frontend.route('/')                                                        
+@frontend.route('/')
 def index():
-    return redirect(url_for('user.posts',page=1))
-
-# 文章主页
-@frontend.route('/posts/pages/<int:page>')
-def posts(page):
     return render_template('index.html')
-# 文章显示
-@frontend.route('/posts/<int:id>')
-def post(id):
-    return render_template('post.html') 
-# 管理页面
-@frontend.route('/admin')
-@login_required
-def admin():
-    return render_template('admin.html')
-# 写文章页面
-@frontend.route('/admin/writer',defaults={'id': ''})
-@frontend.route('/admin/writer/<int:id>')
-@login_required
-def writer(id):
-    return render_template('writer.html')
-# 标签页面
-@frontend.route('/tags',defaults={'key':''})
-@frontend.route('/tags/<string:key>')
-def tag(key):
-    return render_template('tag.html')
-# 分类页面
-@frontend.route('/categories',defaults={'key':''})
-@frontend.route('/categories/<string:key>')
-def category(key):
-    return render_template('category.html')
-# 登录页面
-@frontend.route('/admin/login')
-def login():
-    return render_template('login.html')
-# 404
-@frontend.errorhandler(404)
-def not_found(error):
-    return render_template('404.html')
