61d857ba3a65d50a68441b4c439a4dba36431b65
Pipapa <yezhengmaolove@gmail.com>
2017-11-05 22:16:07 +0800

    权限设置

--- frogblog/api/views.py
+++ frogblog/api/views.py
@@ -36,8 +36,8 @@ def postsList():
         return jsonify(parameter)
     elif request.method == 'POST':
         # 未登录
-        # if current_user.is_authenticated is False:
-        #     abort(403)
+        if current_user.is_authenticated is False:
+            abort(403)
         # 新建文章
         parameter = request.get_json()
         post = parameter['items']
@@ -65,8 +65,8 @@ def postsContent(id):
     # 修改资源
     elif request.method == 'PUT':
         # 未登录
-        # if current_user.is_authenticated is False:
-        #     abort(403)
+        if current_user.is_authenticated is False:
+            abort(403)
         parameter = request.get_json()
         items = parameter['items']
         article = Article.query.get_or_404(id)
